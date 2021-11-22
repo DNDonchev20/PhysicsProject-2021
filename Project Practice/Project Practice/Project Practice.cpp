11992d6e7e7f7ac91c423f2ce77afa326efb6814
Deivid Donchev <85338879+DNDonchev20@users.noreply.github.com>
2021-11-22 21:52:25 +0200

    Add gitignore and second principal of mechanich algorithm

--- Project Practice/Project Practice/Project Practice.cpp
+++ Project Practice/Project Practice/Project Practice.cpp
@@ -67,18 +67,69 @@ int main()
 
 }*/
 
+void second_principle_of_mechanics_check()
+{
+    //дължината
+    double s;
+    cout << "Тhe distance from the starting position of the trolley to the restrictive plates: ";
+    cin >> s;
+    cout << endl;
+
+    //m1 - маса на държателя; m2 = общата маса на количката и тегликите
+    double m1, m2;
+    cout << "The total mass of the holder: "; //общата маса на държателя
+    cin >> m1;
+    cout << endl;
+
+    cout << "The total mass of the trolley: "; //общата маса на количката
+    cin >> m2;
+    cout << endl;
+
+    cout << "The total mass is: "; //oбщата маса е:
+    double m = m1 + m2;//общата маса
+    cout << m << endl;
+
+    cout << "Тhe strength of gravity is equal to the: "; //силата на тежестта е равна на:
+    double F = m1 * 9.8; // = g
+    cout << F << endl;
+
+    double times[10];
+
+    //измерваме три времеna за точност
+    cout << "First time: ";
+    cin >> times[0];
+
+    cout << "Second time: ";
+    cin >> times[1];
+
+    cout << "Third time: ";
+    cin >> times[2];
+    cout << endl;
+
+    //Намираме средноаретмитичното на времената(които са 3)
+    double timeAv = 0;
+    for (int i = 0; i < 3; i++)
+    {
+
+        timeAv += times[i];
+    }
+    timeAv = timeAv / 3;
+
+
+    cout << "Average time: " << timeAv << endl;
+
+    //намираме ускорението по формулата
+    double  acceleration = (2 * s) / pow(timeAv, 2);
+    cout << "Accleration is " << acceleration << endl;
+
+
+    double G = F / acceleration;
+    cout << G;
+
+}
+
 int main()
 {
-    string choise;
-    cin >> choise;
-
-    if (choise == "acceleration")
-    {
-        int acceleration;
-        sloping_plane(acceleration);
-    }
-    /*else if{
-
-    }*/
-    
+   second_principle_of_mechanics_check(); 
 }
+
