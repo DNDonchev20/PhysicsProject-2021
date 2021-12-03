a0fe55b0eb2157ee8f36c885093e288006da116d
TSTranev20 <81413006+TSTranev20@users.noreply.github.com>
2021-12-04 00:13:37 +0200

    Modify header file

--- PhysicsTech/PhysicsTech/PhysicsTech-Functions.h
+++ PhysicsTech/PhysicsTech/PhysicsTech-Functions.h
@@ -1,30 +1,19 @@
 #pragma once
-//headers
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <cstring>
+#include <conio.h>
+
+using namespace std;
+
 float CalculateVolume(float radius, int heightFlask);
-
-float CalculateMass(float F, float acceleration);
-
-float CalculateVolume(float radius, int heightFlask);
-
-void forceOfArchimedes();
-
-float calculateTimeAverage(float times[], int count);
-
-float CalculateAcceleration(float distance, float timeAverage);
 
 float* readTimes(int count);
-
-void secondPrincipleOfMechanicsCheck();
-
-void accelerationsOfPlanets();
 
 void printMenu();
 
 float enterTheShapes(string shapeSelector, float radiusShapes, float aCube, float volumeShape, float heightCylinder);
-
-void printMenuForShapes();
-
-void forceOfArchimedes();
 
 float calculateTimeAverage(float times[], int count);
 
