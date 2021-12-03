175a6b425fb0b3b20b6be391107c2a8b9d0db163
TSTranev20 <81413006+TSTranev20@users.noreply.github.com>
2021-12-04 00:26:48 +0200

    Create test cases for CalculateVolume function, CalculateAcceleration function, CalculateMass function

--- PhysicsTech/UnitTesting/UnitTesting.cpp
+++ PhysicsTech/UnitTesting/UnitTesting.cpp
@@ -21,6 +21,50 @@ namespace UnitTesting
 			Assert::AreEqual(result, expected, L"Expected 3!");
 		}
 
+		TEST_METHOD(TestCalculateMassFunction)
+		{
+			float result;
+			float expected;
+
+			result = CalculateMass(6, 3);
+			expected = 6 / 3;
+
+			Assert::AreEqual(result, expected, L"Expected 2!");
+		}
+
+		TEST_METHOD(TestCalculateMassFunction)
+		{
+			float result;
+			float expected;
+
+			result = CalculateMass(8, 4);
+			expected = 8 / 4;
+
+			Assert::AreEqual(result, expected, L"Expected 2!");
+		}
+
+		TEST_METHOD(TestCalculateAccelerationFunction)
+		{
+			float result;
+			float expected;
+
+			result = CalculateAcceleration(8, 2);
+			expected = (2 * 8) / (2 * 2);
+
+			Assert::AreEqual(result, expected, L"Expected 4!");
+		}
+
+		TEST_METHOD(TestCalculateAccelerationFunction)
+		{
+			float result;
+			float expected;
+
+			result = CalculateAcceleration(16, 4);
+			expected = (2 * 16) / (4 * 4);
+
+			Assert::AreEqual(result, expected, L"Expected 2!");
+		}
+
 		TEST_METHOD(TestCalculateAccelerationFunction)
 		{
 			float result;
@@ -30,6 +74,37 @@ namespace UnitTesting
 			expected = (2 * 4.5) / (3 * 3);
 
 			Assert::AreEqual(result, expected, L"Expected 1!");
+		}
+		
+		TEST_METHOD(TestCalculateVolumeFunction)
+		{
+			float result;
+			float expected;
+
+			result = CalculateVolume(2.5, 3);
+			expected = 3.14f * 2.5 * 2.5 * 3;
+
+			Assert::AreEqual(result, expected, L"Expected 58,875!");
+		}
+		TEST_METHOD(TestCalculateVolumeFunction)
+		{
+			float result;
+			float expected;
+
+			result = CalculateVolume(5, 2);
+			expected = 3.14f * 5 * 5 * 2;
+
+			Assert::AreEqual(result, expected, L"Expected 157!");
+		}
+		TEST_METHOD(TestCalculateVolumeFunction)
+		{
+			float result;
+			float expected;
+
+			result = CalculateVolume(10, 4);
+			expected = 3.14f * 10 * 10 * 4;
+
+			Assert::AreEqual(result, expected, L"Expected 1256!");
 		}
 	};
 }
